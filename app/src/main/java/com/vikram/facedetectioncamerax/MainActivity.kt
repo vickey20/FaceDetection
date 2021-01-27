422489e967f3668bf7b144c78bf0418936e4635e
Vikram Gupta <vikramgupta20.7@gmail.com>
2021-01-26 17:18:35 -0800

    Fix rotation

--- app/src/main/java/com/vikram/facedetectioncamerax/MainActivity.kt
+++ app/src/main/java/com/vikram/facedetectioncamerax/MainActivity.kt
@@ -3,14 +3,16 @@ package com.vikram.facedetectioncamerax
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.*
 import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -18,7 +20,6 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.vikram.facedetectioncamerax.databinding.ActivityMainBinding
-import java.lang.Exception
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity(), ImageCapturedListener {
@@ -30,10 +31,20 @@ class MainActivity : AppCompatActivity(), ImageCapturedListener {
     }
 
     private val cameraExecutor = Executors.newSingleThreadExecutor()
-    private var imageAnalyzer: ImageAnalysis? = null
+    private var imageAnalyzer = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, FaceDetectAnalyzer(this))
+            }
+
     private var preview: Preview? = null
     private var cameraProvider: ProcessCameraProvider? = null
-    private var imageCapture: ImageCapture? = null
+    private var imageCapture = ImageCapture.Builder()
+            .setTargetRotation(Surface.ROTATION_0)
+            .build()
+
+    private var rotation = Surface.ROTATION_270
 
     private lateinit var binding: ActivityMainBinding
 
@@ -43,26 +54,35 @@ class MainActivity : AppCompatActivity(), ImageCapturedListener {
         setContentView(binding.root)
     }
 
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) {
+                    return
+                }
+
+                rotation = when (orientation) {
+                    in 45 until 135 -> Surface.ROTATION_270
+                    in 135 until 225 -> Surface.ROTATION_180
+                    in 225 until 315 -> Surface.ROTATION_90
+                    else -> Surface.ROTATION_0
+                }
+
+                imageCapture.targetRotation = rotation
+            }
+        }
+    }
+
     private fun startCamera() {
+        orientationEventListener.enable()
         val cameraProviderFuture = ProcessCameraProvider.getInstance(baseContext)
         cameraProviderFuture.addListener({
             cameraProvider = cameraProviderFuture.get()
             preview = Preview.Builder().build()
                     .also { it.setSurfaceProvider(binding.preview.surfaceProvider) }
 
-            imageAnalyzer = ImageAnalysis.Builder()
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .build()
-                    .also {
-                        it.setAnalyzer(cameraExecutor, FaceDetectAnalyzer(this))
-                    }
-
             val cameraSelector = CameraSelector.Builder()
                     .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
-                    .build()
-
-            imageCapture = ImageCapture.Builder()
-                    .setTargetRotation(binding.preview.display.rotation)
                     .build()
 
             try {
@@ -92,6 +112,7 @@ class MainActivity : AppCompatActivity(), ImageCapturedListener {
     override fun onPause() {
         super.onPause()
         cameraProvider?.unbindAll()
+        orientationEventListener.disable()
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -116,23 +137,17 @@ class MainActivity : AppCompatActivity(), ImageCapturedListener {
 
     override fun onFaceDetected() {
         takePhoto()
-        imageAnalyzer?.clearAnalyzer()
     }
 
     private fun takePhoto() {
-        imageCapture?.takePicture(cameraExecutor, object : ImageCapture.OnImageCapturedCallback() {
+        imageCapture.takePicture(cameraExecutor, object : ImageCapture.OnImageCapturedCallback() {
             @SuppressLint("UnsafeExperimentalUsageError")
-            override fun onCaptureSuccess(image: ImageProxy) {
-                super.onCaptureSuccess(image)
-                Log.d(TAG, "takePhoto success: image: $image")
-
-                image.image?.let {
-                    val bitmap = it.imageToBitmap()
-                    runOnUiThread {
-                        binding.image.setImageBitmap(bitmap)
-                        binding.progressCircular.visibility = View.GONE
-                        cameraProvider?.unbindAll()
-                    }
+            override fun onCaptureSuccess(imageProxy: ImageProxy) {
+                super.onCaptureSuccess(imageProxy)
+                Log.d(TAG, "takePhoto success: image rotation: ${imageProxy.imageInfo.rotationDegrees}")
+                imageProxy.image?.let {
+                    processAndDisplayImage(imageProxy.imageInfo.rotationDegrees.toFloat(), it)
+                    imageAnalyzer.clearAnalyzer()
                 }
             }
 
@@ -141,6 +156,19 @@ class MainActivity : AppCompatActivity(), ImageCapturedListener {
                 Log.d(TAG, "takePhoto error: ${exception.message}", exception)
             }
         })
+    }
+
+    private fun processAndDisplayImage(rotation: Float, image: Image) {
+        Log.d(TAG, "rotation: $rotation")
+        val bitmap = image
+                .imageToBitmap()
+                .rotateImage(rotation)
+
+        runOnUiThread {
+            binding.image.setImageBitmap(bitmap)
+            binding.progressCircular.visibility = View.GONE
+            cameraProvider?.unbindAll()
+        }
     }
 }
 
@@ -178,17 +206,21 @@ interface ImageCapturedListener {
     fun onFaceDetected()
 }
 
-fun Image.imageToBitmap(): Bitmap? {
+fun Image.imageToBitmap(): Bitmap {
     val buffer = this.planes[0].buffer
     val bytes = ByteArray(buffer.remaining())
     buffer.get(bytes)
 
-    /*val encoded = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        Base64.getEncoder().encodeToString(bytes)
-    } else {
-        TODO("VERSION.SDK_INT < O")
-    }
-    Log.d("MainActivity", "encoded string: $encoded")*/
+    val encoded = Base64.encodeToString(bytes, Base64.DEFAULT)
+    Log.d("MainActivity", "encoded string: $encoded")
 
     return BitmapFactory.decodeByteArray(bytes, 0, bytes.size, null)
+}
+
+fun Bitmap.rotateImage(degree: Float): Bitmap? {
+    val matrix = Matrix().apply {
+        postRotate(degree)
+    }
+
+    return Bitmap.createBitmap(this, 0, 0, width, height, matrix, false)
 }
